3f5783bc613f65f9980f9ad613b4ed45712d125c
Steven Cook <stevenjamescook@gmail.com>
2014-05-23 08:49:24 +0100

    Waters   GET /waters     displays waters

--- spec/requests/waters_spec.rb
+++ spec/requests/waters_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe "Waters" do
-  describe "GET /waters" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get waters_path
-      response.status.should be(200)
+   
+   context "GET /waters" do
+    it "displays waters" do
+      FactoryGirl.create(:water, name: "Fishing Loch", lng: "55.904503" , lat: "-5.941418" )
+      visit waters_path
+      expect(page).to have_content "Fishing Loch"
+      expect(page).to have_content "55.904503,-5.941418"
     end
   end
+
 end
