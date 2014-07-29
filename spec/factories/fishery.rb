6cd3c4c68d6907d41f8799e23679394346e60571
Steven Cook <stevenjamescook@gmail.com>
2014-07-29 22:09:23 +0100

    Added some extra fields to fishery

--- spec/factories/fishery.rb
+++ spec/factories/fishery.rb
@@ -1,16 +1,22 @@
 FactoryGirl.define do
-   factory :fishery do
-    name Faker::Company.name
-    contact_name Faker::Name.name 
-    street Faker::Address.street_address
-    line2 Faker::Address.street_address
-    country Faker::Address.country
-    region Faker::Address.state
-    telephone Faker::PhoneNumber.phone_number
-    postcode Faker::Address.postcode
-    email Faker::Internet.email
-    website Faker::Internet.url
-    lat Faker::Address.latitude
-    lng Faker::Address.longitude
-  end
+    factory :fishery do
+        name Faker::Company.name
+        contact_name Faker::Name.name 
+        street Faker::Address.street_address
+        line2 Faker::Address.street_address
+        country Faker::Address.country
+        region Faker::Address.state
+        telephone Faker::PhoneNumber.phone_number
+        mobile Faker::PhoneNumber.cell_phone
+        postcode Faker::Address.postcode
+        email Faker::Internet.email
+        website Faker::Internet.url
+        lat Faker::Address.latitude
+        lng Faker::Address.longitude
+        description Faker::Lorem.sentence
+        prices Faker::Commerce.price
+        record Faker::Lorem.sentence
+        season Faker::Lorem.sentence
+        facilities Faker::Lorem.sentence
+    end
 end
