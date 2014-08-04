a5d0b718081f68cf0a2da532a171ede5f7bb31aa
Steven Cook <stevenjamescook@gmail.com>
2014-08-04 23:42:41 +0100

    added missing fields. Passing tests

--- spec/requests/fisheries_spec.rb
+++ spec/requests/fisheries_spec.rb
@@ -14,10 +14,12 @@ describe "Fisheries", :type => :request do
 
         fill_in 'fishery_name', :with => "Bob's mega fishy fishery"
         fill_in 'fishery_contact_name', :with => "Bob is the contact name"
+        fill_in 'fishery_lat_lng', :with => "51.91102240717697, -1.8956728515624945"
         fill_in 'fishery_street', :with => "Fishery Street"
         fill_in 'fishery_line2', :with => "Fishery line 2"
         fill_in 'fishery_region', :with => "Fishery Region"
         fill_in 'fishery_telephone', :with => "12345678"
+        fill_in 'fishery_mobile', :with => "8989889"
         fill_in 'fishery_postcode', :with => "Fishery Postcode"
         fill_in 'fishery_description', :with => "Three spring-fed trout lakes totalling two acres"
         fill_in 'fishery_prices', with: "8hrs £20 (4 fish); 6hrs £17.50 (3fish);"
@@ -26,9 +28,21 @@ describe "Fisheries", :type => :request do
         fill_in 'fishery_facilities', with: "Lodge, tackle shop and toilets"
 
         click_on 'Submit'
-        
+
         expect(page).to have_content "Bob's mega fishy fishery"
         expect(page).to have_content "Bob is the contact name"
+        expect(page).to have_content "51.91102240717697, -1.8956728515624945"
+        expect(page).to have_content "Fishery Street"
+        expect(page).to have_content "Fishery line 2"
+        expect(page).to have_content "Fishery Region"
+        expect(page).to have_content "12345678"
+        expect(page).to have_content "8989889"
+        expect(page).to have_content "Fishery Postcode"
+        expect(page).to have_content "Three spring-fed trout lakes totalling two acres"
+        expect(page).to have_content "8hrs £20 (4 fish); 6hrs £17.50 (3fish);"
+        expect(page).to have_content "Rainbow 26lb 4oz"
+        expect(page).to have_content "All season"
+        expect(page).to have_content "Lodge, tackle shop and toilets"
       end
 
       it "Updates a fishery's details" do
@@ -50,6 +64,7 @@ describe "Fisheries", :type => :request do
         expect(page).to have_content fishery.street
         expect(page).to have_content fishery.line2
         expect(page).to have_content fishery.region
+        expect(page).to have_content fishery.lat_lng
         expect(page).to have_content fishery.telephone
         expect(page).to have_content fishery.mobile
         expect(page).to have_content fishery.postcode
