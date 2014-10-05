febad2f4536fb2349f695fdbf7610de4c83eb962
Steven Cook <stevenjamescook@gmail.com>
2014-10-05 09:46:31 +0100

    created contact_details, other_details and location methods

--- app/models/fishery.rb
+++ app/models/fishery.rb
@@ -5,4 +5,41 @@ class Fishery < ActiveRecord::Base
   validates_presence_of :name
 
 
+  def contact_details
+    return_filled_fields ['contact_name', 'telephone', 'mobile', 'email', 'website']
+  end
+
+  def other_details
+    return_filled_fields ['records', 'season', 'facilities', 'prices']
+  end
+
+  def location
+    location = { "Address" => address }
+    if lat_lng?
+      location['Latitude']  = lat_lng.split(',')[0]
+      location['Longitude'] = lat_lng.split(',')[1]
+    end
+    location
+  end
+
+  def address
+    return_filled_fields(['street', 'line2', 'region', 'country', 'postcode']).values.join(', ')
+  end
+
+  private
+
+  def return_filled_fields(fields)
+    filled_fields = {}
+      fields.each do | field |
+        if send("#{field}?")
+          filled_fields[ human_readable_format field ] = send("#{field}")
+        end
+      end
+    filled_fields
+  end
+
+  def human_readable_format(string)
+    self.class.human_attribute_name string
+  end
+  
 end
--- spec/models/fishery_spec.rb
+++ spec/models/fishery_spec.rb
@@ -2,28 +2,90 @@ require 'rails_helper'
 
 describe Fishery, :type => :model do
   
-  before(:each) do
-    @fishery = Fishery.new
+  before(:all) do
+    @fishery = FactoryGirl.create(
+        :fishery, 
+        #contact details
+        contact_name: 'Steven Cook', 
+        telephone: '',
+        mobile: '',
+        email: 'stevenjamescook@gmail.com',
+        website: 'www.stevencook.com',
+        #other details
+        records: '23lb Rainbow', 
+        season: 'March - September',
+        facilities: '',
+        prices: '£25 a day', 
+        #location
+        street: 'Jubilee Street',
+        line2: 'Langworthy',
+        region: 'Greater Manchester',
+        country: 'England',
+        postcode: 'M6 5EP',
+        lat_lng: '54.43869834845736,-2.2472353515624945'
+      )
   end
 
   it "is not valid without a name" do
+
     @fishery.name = nil
     expect(@fishery).not_to be_valid
+
   end
 
-  describe "#formatted_fields" do
+  describe "#contact_details" do
     
-    it "returns all fields that are not empty as a hash" do
-      fishery = FactoryGirl.create(:fishery, name: "Stevo's Big trout fishery")
-      # fishery.id
-      # expect(fishery.formatted_fields).to eql ({ "description" => "test"})
+    it "returns a hash of 'contact details' which are not empty" do
+      
+      expect(@fishery.contact_details).to eql (
+        { 
+          'Contact name' => @fishery.contact_name,
+          'Email'        => @fishery.email,
+          'Website'      => @fishery.website
+        }
+      )
+
     end
   end
 
-  describe "#formatted_address" do
-    it "returns a formatted address" do
+  describe "#other_details" do
+    
+    it "returns a hash of 'other details' which aren't empty" do
+
+      expect(@fishery.other_details).to eql (
+        { 
+          'Records' => @fishery.records,
+          'Season'  => @fishery.season,
+          'Prices'  => @fishery.prices
+        }
+      )
 
     end
   end
 
+  describe "#location" do
+    
+    it "returns a hash of 'location details' which aren't empty" do
+
+      expect(@fishery.location).to eql (
+        {
+          'Address' => 'Jubilee Street, Langworthy, Greater Manchester, England, M6 5EP',
+          'Latitude' => '54.43869834845736',
+          'Longitude' => '-2.2472353515624945'
+        }
+      )
+
+    end
+
+  end
+
+  describe "#address" do
+
+    it "returns the full fishery address concatenate into one string" do
+      
+      expect(@fishery.address).to eql ('Jubilee Street, Langworthy, Greater Manchester, England, M6 5EP')
+
+    end
+
+  end
 end
