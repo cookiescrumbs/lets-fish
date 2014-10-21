14ffc334274dd69bf976072deb81cd7bc7fe1481
James Ruston <jruston90@gmail.com>
2014-10-21 09:46:08 +0100

    set all dates to 2012

--- app/models/water.rb
+++ app/models/water.rb
@@ -6,7 +6,11 @@ class Water < ActiveRecord::Base
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 
   def season_start=(value)
-   super(Date.parse(value).change({year: 2012}))
+    super(Date.parse(value).change({year: 2012}))
+  end
+
+  def season_end=(value)
+    super(Date.parse(value).change({year: 2012}))
   end
 
 end
