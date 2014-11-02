73d6f1fcdfebeb80cf60bed07ca9908566f65dd9
Steven Cook <stevenjamescook@gmail.com>
2014-11-02 18:03:09 +0000

    add no fishery message

--- spec/requests/admin/waters_page_spec.rb
+++ spec/requests/admin/waters_page_spec.rb
@@ -1,26 +1,30 @@
 describe "Manage waters page", :type => :request do
 
-  before(:each) do
-    @fishery = FactoryGirl.create :fishery_with_waters
-    FactoryGirl.create :water
-    visit admin_fishery_waters_path @fishery.id
+  context "there is a fishery with waters" do
+
+    before(:each) do
+      @fishery = FactoryGirl.create :fishery_with_waters
+      FactoryGirl.create :water
+      visit admin_fishery_waters_path @fishery.id
+    end
+
+    let(:number_of_waters){ @fishery.waters.count }
+
+    it "lists a fisheries waters" do
+      expect(page.all('table tr.water').count).to eql number_of_waters
+    end
+
+    it "has a link to create a new water" do
+      expect(page).to have_link 'New Water', href: new_admin_fishery_water_path(@fishery)
+    end
+
+    it "has the name of the fishery on the page" do
+      expect(page).to have_content "#{@fishery.name.possessive} waters"
+    end
+
   end
 
-  let(:number_of_waters){ @fishery.waters.count }
-
-  it "lists a fisheries waters" do
-    expect(page.all('table tr.water').count).to eql number_of_waters
-  end
-
-  it "has a link to create a new water" do
-    expect(page).to have_link 'New Water', href: new_admin_fishery_water_path(@fishery)
-  end
-
-  it "has the name of the fishery on the page" do
-    expect(page).to have_content "#{@fishery.name.possessive} waters"
-  end
-
-  context "There are no waters" do
+  context "there is a fishery without waters" do
 
     before(:each) do
       @fishery_without_waters = FactoryGirl.create :fishery
