d9073d4a98bcf3d88e4f3c9f691a1f69c221e15b
Steven Cook <stevenjamescook@gmail.com>
2014-12-03 22:47:56 +0000

    responsive map

--- spec/requests/admin/admin_layout_spec.rb
+++ spec/requests/admin/admin_layout_spec.rb
@@ -4,8 +4,27 @@ describe "Admin layout", type: feature do
     visit '/admin/fisheries'
   end
 
+  it "has a page title" do
+    expect page.has_title? 'Gone Fishing Admin'
+  end
+
   it "has a page heading" do
     expect(page).to have_css '.navbar-brand', 'Gone Fishing Admin'
   end
+
+  describe "Navigation" do
+
+    let(:nav_bar) { page.find('.navbar') }
+
+    it "has a link to fisheries index" do
+      expect(nav_bar.has_link? "Fisheries", href: 'admin/fisheries')
+    end
+
+    it "has a sign out link" do
+      expect(nav_bar.has_link? "Sign out")
+    end
+    
+  end
+
 
 end
