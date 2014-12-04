911473867a8ff44d6b87a5fa83209df864f854ba
Steven Cook <stevenjamescook@gmail.com>
2014-12-04 23:03:49 +0000

    fudged the js fixed admin layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery-1.11.0
 //= require jquery_ujs
 //= require bootstrap.min
-//= require cbpAnimatedHeader.min
 //= require classie
+//= require cbpAnimatedHeader
 //= require main
-//= require_tree .
