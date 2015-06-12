7a2ace80be8d22fb3f136d1b7ee883371c6d4086
Steven Cook <stevenjamescook@gmail.com>
2015-06-12 16:42:11 +0100

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ GoneFishing::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
+
   config.assets.prefix = '/shared/assets'
 
   # Compress JavaScripts and CSS.
