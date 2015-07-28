d917339e9e6239b3cfc266c9455b5d29537b22b9
Steven Cook <stevenjamescook@gmail.com>
2015-07-28 19:54:45 +0100

    css fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_page_id
+
+  def set_page_id
+    @page_id = 'id='+params[:controller]+'' unless params[:controller].empty?
+  end
+
 end
