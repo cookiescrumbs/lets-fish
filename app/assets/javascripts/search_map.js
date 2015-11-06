b29918ac2c2dc1d9762a56ee4081ddba5f8360f4
Steven Cook <stevenjamescook@gmail.com>
2015-11-06 12:41:18 +0000

    stuff

--- app/assets/javascripts/search_map.js
+++ app/assets/javascripts/search_map.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   var map,
+  image,
+  shape,
   mapOptions = {
     mapTypeControlOptions: {
       style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
@@ -12,6 +14,21 @@ $(document).ready(function() {
   },
   markers =[],
   boundingBox;
+
+  var image = {
+    url: "/images/pin.png",
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32)
+  };
+
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
 
   //make the a new instance of google maps
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
