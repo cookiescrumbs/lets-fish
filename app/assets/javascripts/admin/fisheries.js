fd0b4ae75e0657d7a83da28af8a9bee659324950
Steven Cook <stevenjamescook@gmail.com>
2015-12-24 09:31:43 +0000

    JS files per admin controller Fixed Turbo links

--- app/assets/javascripts/admin/fisheries.js
+++ app/assets/javascripts/admin/fisheries.js
@@ -9,9 +9,4 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require bootstrap.min
-//= require turbolinks
+//= require place_details
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-GoneFishing::Application.config.assets.precompile += %w( homepage.js search.js admin.css admin.js )
+GoneFishing::Application.config.assets.precompile += %w( homepage.js search.js admin.css admin.js admin/waters.js admin/fisheries.js)
