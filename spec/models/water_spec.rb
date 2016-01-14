683362ccf7a36d01270e05fa4ecdb9cffc7403ba
Steven Cook <stevenjamescook@gmail.com>
2016-01-14 18:40:43 +0000

    added range

--- spec/models/water_spec.rb
+++ spec/models/water_spec.rb
@@ -52,8 +52,44 @@ describe Water, :type => :model do
     describe "Address" do
 
       it "is the 'formatted address' from google maps goecode api" do
-        expect(@water.address).to eql("Manchester, UK")
+        expect(@water.address).to eql "Manchester, UK"
       end
+
+    end
+
+    describe "short address", focus: true do
+
+      it "handles an address with a postcode" do
+        @water.address = "Unnamed Road, Morpeth, Northumberland NE65 7LG, UK"
+        expect(@water.short_address).to eql "Morpeth, Northumberland"
+      end
+
+      it "handles an address with no elements" do
+        @water.address = ""
+        expect(@water.short_address).to eql nil
+      end
+
+      it "handles an address with one element" do
+        @water.address = "Northumberland"
+        expect(@water.short_address).to eql "Northumberland"
+      end
+
+      it "handles an address with two elements and no postcode" do
+        @water.address = "Unnamed Road, Northumberland"
+        expect(@water.short_address).to eql "Northumberland"
+      end
+
+      it "handles an address with three element and no postcode " do
+        @water.address = "Unnamed Road, Northumberland, UK"
+        expect(@water.short_address).to eql "Northumberland, UK"
+      end
+
+     it "handles an address with four elements and no postcode" do
+        @water.address = "Unnamed Road, Cumnor, Oxfordshire, UK"
+        expect(@water.short_address).to eql "Cumnor, Oxfordshire"
+     end
+
+
 
     end
 end
