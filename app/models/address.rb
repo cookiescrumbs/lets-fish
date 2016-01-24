8bf5fa08d584314d0cf5d46ad9c894a6718f2b91
Steven Cook <stevenjamescook@gmail.com>
2016-01-24 10:11:36 +0000

    formatted address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ActiveRecord::Base
   belongs_to :fishery
+
+  def formatted
+    ['street','line2','region','postcode','country'].select do |address_comp|
+      !send(address_comp).blank?
+    end.map {|address_comp| send(address_comp) }.join(', ')
+  end
 end
