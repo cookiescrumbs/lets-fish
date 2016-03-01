ac14ec16bc9dbfea1261473e6e39b018b70178fb
Steven Cook <stevenjamescook@gmail.com>
2016-03-01 21:28:44 +0000

    spike

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -4,6 +4,7 @@ class WatersController < ApplicationController
   before_action :set_waters, only: [:show]
   before_action :set_fishery, only: [:show]
   before_action :set_species, only: [:show]
+  before_action :set_image_attribution, only: [:show]
 
 
   def show
@@ -13,13 +14,36 @@ class WatersController < ApplicationController
     def set_water
       @water = Water.find_by(slug: params[:id])
     end
+
     def set_waters
       @waters = Water.find_by(slug: params[:id]).fishery.waters
     end
+
     def set_fishery
       @fishery = Water.find_by(slug: params[:id]).fishery
     end
+
     def set_species
       @species = Water.find_by(slug: params[:id]).species
     end
+
+    def set_image_attribution
+      binding.pry
+      if !geograph_photo_id.nil?
+        @image_attribution = GeographService::user_attribution_from geograph_photo_id
+      end
+    end
+
+    def geograph_photo_id
+      first_image.geograph_photo_id unless first_image.nil? || first_image.geograph_photo_id.nil?
+    end
+
+    def images
+      Water.find_by(slug: params[:id]).images
+    end
+
+    def first_image
+      images.first unless images.first.nil?
+    end
+
 end
