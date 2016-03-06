0011da8dfbb1c0b68e11aae7893438e99095b2a7
Steven Cook <stevenjamescook@gmail.com>
2016-03-06 15:59:24 +0000

    added api key to tests

--- spec/support/web_mock_helpers.rb
+++ spec/support/web_mock_helpers.rb
@@ -2,12 +2,12 @@ module WebMockHelpers
   module GoogleApis
     def stub_google_geocode
       body = File.read("spec/fixtures/googleapis/geocode/manchester.json")
-      stub_request(:get, /http:\/\/maps.googleapis.com\/maps\/api\/geocode\/json\?language=en&latlng=(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)&sensor=false/).
+      stub_request(:get, /https:\/\/maps.googleapis.com\/maps\/api\/geocode\/json\?key=AIzaSyAbQz9wmM_wnPdi4Q8-dlbDOyDeS10zb58&language=en&latlng=(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)&sensor=false/).
          to_return(:status => 200, :body => body, :headers => {})
     end
     def stub_google_geocode_address
       body = File.read("spec/fixtures/googleapis/geocode/manchester.json")
-      stub_request(:get, "http://maps.googleapis.com/maps/api/geocode/json?address=manchester&language=en&sensor=false").
+      stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=manchester&key=AIzaSyAbQz9wmM_wnPdi4Q8-dlbDOyDeS10zb58&language=en&sensor=false").
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => body, :headers => {})
     end
