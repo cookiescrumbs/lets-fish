1a523a77e39ee90e0bc9b32b3ae485cb5e7bf262
Steven Cook <stevenjamescook@gmail.com>
2016-03-07 18:41:36 +0000

    no message

--- spec/services/geograph_service_spec.rb
+++ spec/services/geograph_service_spec.rb
@@ -7,15 +7,16 @@ describe GeographService, ".user_attribution_from photo_id:" do
   end
 
   let(:photo_id) { '158570' }
+  let(:api_key) { '1234apikey'}
 
   it "returns a hash containing details of a user's attribution from a photo id" do
-    user_attribution = GeographService::user_attribution_from photo_id
+    user_attribution = GeographService::user_attribution_from photo_id, api_key
     expect(user_attribution).to eql(
         {
-          name:"Theo Pike",
-          profile_url:"http://www.geograph.org.uk/profile/123456",
+          name: "Theo Pike",
+          profile_url: "http://www.geograph.org.uk/profile/123456",
           photo_url: "http://www.geograph.org.uk/photo/158570",
-          licence:"http://creativecommons.org/licenses/by-sa/2.0/"
+          licence: "http://creativecommons.org/licenses/by-sa/2.0/"
         }
     )
   end
