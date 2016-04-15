ea8d0c6d98321bb1245e15fb06b9d050b4c5b0ca
Steven Cook <stevenjamescook@gmail.com>
2016-04-15 11:18:31 +0100

    Added point and save to local storage

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -9,4 +9,6 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require mustache
 //= require trip_map
+
