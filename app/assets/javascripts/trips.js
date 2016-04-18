112c82fb6b7b78787d8510a2b59bb76245e6a6af
Steven Cook <stevenjamescook@gmail.com>
2016-04-18 19:03:08 +0100

    added editable

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -9,7 +9,7 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery.editable.min.js
+//= require bootstrap3-editable-1.5.1/bootstrap3-editable/js/bootstrap-editable.min
 //= require mustache
 //= require trip_map
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module LetsFish
   class Application < Rails::Application
+     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,5 +22,6 @@ module LetsFish
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = true
     config.serve_static_assets = true
+
   end
 end
