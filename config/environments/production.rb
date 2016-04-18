5e12f9200874401386ea5d188231ee420580d539
Steven Cook <stevenjamescook@gmail.com>
2016-04-18 22:43:05 +0100

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,9 @@ LetsFish::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  # config.assets.compile = true
-  # config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  #config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
