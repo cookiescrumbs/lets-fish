ba83a7fb4c78ec95cb6640002181fc532e5120e0
Steven Cook <stevenjamescook@gmail.com>
2016-04-19 11:33:06 +0100

    asset shit line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,14 @@ LetsFish::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   # config.serve_static_assets = true
 
-  config.assets.prefix = '/shared/assets'
+  # config.assets.prefix = '/assets'
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-    config.assets.compile = false
+  # config.assets.compile = false
   # config.assets.compile = true
   # config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
