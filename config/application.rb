f5ee18556e3b186e4e0c56bff5a8298a1f12ee90
Steven Cook <stevenjamescook@gmail.com>
2016-04-19 10:07:27 +0100

    fonts fonts fonts

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(:default, Rails.env)
 
 module LetsFish
   class Application < Rails::Application
-    config.assets.enabled = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
