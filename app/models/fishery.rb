01040e060003604ace9c01e8dcfae772b9301ce0
Steven Cook <stevenjamescook@gmail.com>
2016-04-20 12:16:52 +0100

    Added google places details

--- app/models/fishery.rb
+++ app/models/fishery.rb
@@ -11,4 +11,23 @@ class Fishery < ActiveRecord::Base
   has_one  :address
   accepts_nested_attributes_for :address
 
+  #Move this stuff out into an object
+
+  def latitude
+    google_places_details["geometry"]["location"]["lat"] if google_places_details?
+  end
+
+  def longitude
+    google_places_details["geometry"]["location"]["lng"] if google_places_details?
+  end
+
+  def google_places_details
+    return nil if place_id.blank?
+    Geocoder.search(place_id, :lookup => :google_places_details).first.data
+  end
+
+  def google_places_details?
+    !place_id.blank?
+  end
+
 end
