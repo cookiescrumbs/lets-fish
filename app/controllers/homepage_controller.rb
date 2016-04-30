a82e7fe42403183d58f9ae63728d7bb73ed91e7a
Steven Cook <stevenjamescook@gmail.com>
2016-04-30 16:15:30 +0100

    fresh_when caching for the homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,18 +1,11 @@
 class HomepageController < ApplicationController
 
   def index
-    @waters = recently_add_waters 3
-    @fisheries = recently_add_fisheries 3
-  end
+    @waters = Water.order('id desc').limit(3)
+    fresh_when @waters
 
-  private
-
-  def recently_add_waters(number)
-    Water.limit(number).order('id desc')
-  end
-
-  def recently_add_fisheries(number)
-    Fishery.limit(number).order('id desc')
+    @fisheries =  Fishery.order('id desc').limit(3)
+    fresh_when @fishery
   end
 
 end
