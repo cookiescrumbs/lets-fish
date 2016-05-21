13162cc26ce31e1e4b8052e359b44ae1ec8b9108
Steven Cook <stevenjamescook@gmail.com>
2016-05-21 09:46:18 +0100

    Removed api keys in favour of regex

--- spec/support/web_mock_helpers.rb
+++ spec/support/web_mock_helpers.rb
@@ -4,20 +4,20 @@ module WebMockHelpers
 
     def stub_google_geocode_lat_lng
       body = File.read("spec/fixtures/googleapis/geocode/manchester.json")
-      stub_request(:get, /https:\/\/maps.googleapis.com\/maps\/api\/geocode\/json\?key=AIzaSyAbQz9wmM_wnPdi4Q8-dlbDOyDeS10zb58&language=en&latlng=(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)&sensor=false/).
+      stub_request(:get, /https:\/\/maps.googleapis.com\/maps\/api\/geocode\/json\?key=([\w\W]+)&language=en&latlng=(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)&sensor=false/).
          to_return(:status => 200, :body => body, :headers => {})
     end
 
     def stub_google_geocode_address
       body = File.read("spec/fixtures/googleapis/geocode/manchester.json")
-      stub_request(:get, /https:\/\/maps.googleapis.com\/maps\/api\/geocode\/json\?address=([\w\W]+)&key=AIzaSyAbQz9wmM_wnPdi4Q8-dlbDOyDeS10zb58&language=en&sensor=false/).
+      stub_request(:get, /https:\/\/maps.googleapis.com\/maps\/api\/geocode\/json\?address=([\w\W]+)&key=([\w\W]+)&language=en&sensor=false/).
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => body, :headers => {})
     end
 
     def stub_google_places_details
       body = File.read("spec/fixtures/googleapis/place/stocks_fly_fishery.json")
-      stub_request(:get, /https:\/\/maps.googleapis.com\/maps\/api\/place\/details\/json\?key=AIzaSyAbQz9wmM_wnPdi4Q8-dlbDOyDeS10zb58&language=en&placeid=([a-zA-Z{0-9}])/).
+      stub_request(:get, /https:\/\/maps.googleapis.com\/maps\/api\/place\/details\/json\?key=([\w\W]+)&language=en&placeid=([a-zA-Z{0-9}])/).
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => body, :headers => {})
     end
@@ -27,7 +27,7 @@ module WebMockHelpers
   module Geograph
     def stub_geograph_photo_details
       body = File.read("spec/fixtures/geograph/photo-details.json")
-      stub_request(:get, /http:\/\/api.geograph.org.uk\/api\/photo\/([\d]+)\?key=a5ecd893c8&output=json/).
+      stub_request(:get, /http:\/\/api.geograph.org.uk\/api\/photo\/([\d]+)\?key=([\w\W]+)&output=jso/).
       with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => body , :headers => {'Content-Type' => 'application/json', })
     end
