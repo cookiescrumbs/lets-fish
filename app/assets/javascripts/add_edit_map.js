c791426c8e47b18b7556353b931919217afa31f2
Steven Cook <stevenjamescook@gmail.com>
2016-08-22 08:44:48 +0100

    Added large orange marker to the add / edit map

--- app/assets/javascripts/add_edit_map.js
+++ app/assets/javascripts/add_edit_map.js
@@ -35,6 +35,7 @@ $(document).ready(function() {
 
       marker = new google.maps.Marker({
         position: getMarkerPosition(map),
+        icon: "https://chart.googleapis.com/chart?chst=d_map_spin&chld=1|0|fed136|13|_|",
         map: map,
         draggable: true
       });
