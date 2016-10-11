2db8e199cc4c4b822b5d3f687856e55981d4130d
Steven Cook <stevenjamescook@gmail.com>
2016-10-11 23:14:17 +0100

    species

--- spec/features/admin/species_spec.rb
+++ spec/features/admin/species_spec.rb
@@ -2,9 +2,11 @@ require_relative '../features_helper'
 
 describe 'Species', type: :feature do
   before(:each) do
+    stub_google_geocode_lat_lng
     stub_google_geocode_address
-    @fishery_manager = FactoryGirl.create :user, email: 'fishery_manager@fishery.com', password: '5lbBr0wnTr0ut'
-    login @fishery_manager
+    @fishery_manager = FactoryGirl.create :user, email: 'fishery_manager@fishery.com', password: '5lbBr0wnTr0ut', auth: Rails.application.config.fishery_manager
+
+    sign_in @fishery_manager
   end
 
   describe 'Manage species' do
@@ -22,19 +24,15 @@ describe 'Species', type: :feature do
     end
 
     it 'delete a species' do
-      FactoryGirl.create(:species, name: 'rainbow trout')
       visit admin_species_index_path
-      click_on 'Destroy'
-      expect(page).not_to have_content 'rainbow trout '
+      page.all('a', text: 'Destroy')[0].click
+      expect(page).not_to have_content 'brown trout'
     end
 
     it 'show a species details' do
-      FactoryGirl.create(:species, name: 'rainbow trout')
-      FactoryGirl.create(:species, name: 'brown trout')
       visit admin_species_index_path
       page.all('a', text: 'Show')[0].click
-      expect(page).to have_content 'rainbow trout'
-      expect(page).not_to have_content 'brown trout'
+      expect(page).to have_content 'brown trout'
     end
   end
 end
