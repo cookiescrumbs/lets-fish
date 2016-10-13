4dc146ee10ef30446c57c7525994c9516266aed8
Steven Cook <stevenjamescook@gmail.com>
2016-10-13 17:53:06 +0100

    added jquery_ujs

--- app/assets/javascripts/admin/waters.js
+++ app/assets/javascripts/admin/waters.js
@@ -9,4 +9,5 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require jquery_ujs
 //= require add_edit_map
