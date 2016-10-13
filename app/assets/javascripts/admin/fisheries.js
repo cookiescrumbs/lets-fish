a35a7de565ceb7ac5a4071a2f5acdf2be3f5b514
Steven Cook <stevenjamescook@gmail.com>
2016-10-13 19:53:04 +0100

    assets

--- app/assets/javascripts/admin/fisheries.js
+++ app/assets/javascripts/admin/fisheries.js
@@ -9,5 +9,4 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery_ujs
 //= require place_details
