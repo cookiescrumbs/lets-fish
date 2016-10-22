512888327ec1ec53665be33f31d912e4eea679d8
Steven Cook <stevenjamescook@gmail.com>
2016-10-22 12:20:30 +0100

    removed map from mobile view

--- app/assets/javascripts/toggle_between_map_and_list.js
+++ app/assets/javascripts/toggle_between_map_and_list.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
     $('#map-list-toggle-bar').hide();
 
     if($( window ).width() < 768) {
-        $('#map-list-toggle-bar').show();
-        $('#results-container').hide();
+        // $('#map-list-toggle-bar').show();
+        $('#map').hide();
     }
 
-    $('button.map').click(function(){
-        $('#results-container').hide();
-        $('#map').show();
-    });
+    // $('button.map').click(function(){
+    //     $('#results-container').hide();
+    //     $('#map').show();
+    // });
 
-    $('button.list').click(function(){
-        $('#map').hide();
-        $('#results-container').show();
-    });
+    // $('button.list').click(function(){
+    //     $('#map').hide();
+    //     $('#results-container').show();
+    // });
 });
