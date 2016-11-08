c7f0e47ccd1f696b67ca5b6dd81cab717eb1e743
Steven Cook <stevenjamescook@gmail.com>
2016-11-08 23:23:05 +0000

    google api key

--- app/services/google_places_service.rb
+++ app/services/google_places_service.rb
@@ -1,7 +1,7 @@
 module GooglePlacesService
-  API_KEY = Rails.application.config.dark_sky_api_key
+  API_KEY = Rails.application.config.google_api_key
   def self.places(lat:, lng:)
     google_places = GooglePlaces::Client.new(GooglePlacesService::API_KEY)
-    google_places.spots(lat,lng, types: ['lodging', 'campground'], radius: five_miles )
+    google_places.spots(lat,lng, types: ['lodging', 'campground'], radius: 17000 )
   end
 end
