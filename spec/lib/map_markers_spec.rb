e407e715b915d37cea5fe1f330f76502cb4839ab
Steven Cook <stevenjamescook@gmail.com>
2016-11-14 08:30:45 +0000

    map marker specs

--- spec/lib/map_markers_spec.rb
+++ spec/lib/map_markers_spec.rb
@@ -7,7 +7,8 @@ describe MapMarkers::Places, '.build', focus: true do
     places = [*1..5].map do
       Place.new(id: 12345, name: 'a name', lat: 54.665559, lng: -2.299508, types: ['lodging', 'establishment', 'site_of_interest'])
     end
-    @markers = MapMarkers::Places::build(places)
+    type = 'lodging'
+    @markers = MapMarkers::Places::build(places, type)
   end
 
   it 'returns an array of PlaceMarker objects' do
@@ -18,16 +19,17 @@ describe MapMarkers::Places, '.build', focus: true do
   context 'missing data' do
     before(:each) do
       places = [*1..5].map do
-        Place.new(id: 12345, name: 'a name', lat: 54.665559, lng: -2.299508)
+        Place.new(id: 12345, lat: 54.665559, lng: -2.299508)
       end
-      @markers = MapMarkers::Places::build(places)
+      type = 'lodging'
+      @markers = MapMarkers::Places::build(places, type)
     end
 
-    it 'returns an array of PlaceMarker objects without a type' do
+    it 'returns an array of PlaceMarker objects with name missing' do
       expect(@markers.length).to eql 5
       expect(@markers.first.class).to eql PlaceMarker
+      expect(@markers.first.name).to eql nil
     end
   end
-
 
 end
