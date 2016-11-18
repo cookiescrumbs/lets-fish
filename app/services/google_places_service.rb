6044f6cdaa0993969897d57d3d565862a8bed9b2
Steven Cook <stevenjamescook@gmail.com>
2016-11-18 15:55:16 +0000

    started

--- app/services/google_places_service.rb
+++ app/services/google_places_service.rb
@@ -5,9 +5,10 @@ module GooglePlacesService
   base_uri 'https://maps.googleapis.com'
   API_KEY = Rails.application.config.google_api_key
 
-  def self.places(lat:, lng:, type:)
+  def self.places(lat:, lng:, type:, zoom:)
     begin
-      response = get("/maps/api/place/nearbysearch/json?key=#{GooglePlacesService::API_KEY}&location=#{lat},#{lng}&radius=17000&types=#{type}", timeout: 1)
+      radius = self.zoom_to_radius
+      response = get("/maps/api/place/nearbysearch/json?key=#{GooglePlacesService::API_KEY}&location=#{lat},#{lng}&radius=#{radius}&types=#{type}", timeout: 1)
       return {} unless response.code === 200
       PlacesBuilder::build(response['results'])
     rescue
@@ -23,6 +24,31 @@ module GooglePlacesService
     rescue
       {}
     end
+  end
+
+
+  def self.zoom_to_radius(zoom)
+
+    if zoom.between?(1, 5)
+      34000  #20 miles
+    end
+
+    if zoom.between?(6, 10)
+      17000  #10 miles
+    end
+
+
+    1  World
+    5 Landmass/continent
+    10 City
+    15 Streets
+    20 Buildings
+
+    34000  20 miles
+    17000  10 miles
+    8500   5 miles
+
+
   end
 
 end
