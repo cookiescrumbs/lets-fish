a8a2df40e0cb2052cf6e2fe66cb68d6607610c7b
Steven Cook <stevenjamescook@gmail.com>
2016-11-25 11:17:02 +0000

    open graph for waters

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -46,9 +46,24 @@ class WatersController < ApplicationController
     !Water.friendly.find(params[:id]).images.first.geograph_photo_id.nil?
   end
 
+#todo ... Move this into a helper or somthing!!
   def set_meta
     water = Water.friendly.find(params[:id])
     set_meta_tags title: water.meta_title
     set_meta_tags description: water.meta_description
+    set_meta_tags og: {
+      title:    water.meta_title,
+      type:     'place',
+      place: {
+        location: {
+          latitude: water.latitude ,
+          longitude: water.longitude
+        }
+      },
+      description: water.meta_description.slice(0..160),
+      url: request.original_url,
+      image: (water.images.first)? water.images.first.image.url(:medium) : '/images/missing.png',
+      site_name: 'Let\'s Fish'
+    }
   end
 end
