6aceba7d05793ea93e72af031671f82a5ffee4db
Steven Cook <stevenjamescook@gmail.com>
2016-12-08 17:27:26 +0000

    og default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,14 @@ module ApplicationHelper
   def default_meta_tags
     {
       title:       "Fly fishing lakes, lochs, rivers and streams - let's fish",
-      description: "Discover new flying fishing destinations from around the British Isles with Let's fish."
+      description: "Discover new flying fishing destinations from around the British Isles with Let's fish.",
+      og: {
+        title: "Fly fishing lakes, lochs, rivers and streams - let's fish",
+        description: "Discover new flying fishing destinations from around the British Isles with Let's fish.",
+        url: 'http://lets.fish',
+        image: '/images/header-bg.jpg',
+        site_name: 'Let\'s Fish'
+      }
     }
   end
 end
