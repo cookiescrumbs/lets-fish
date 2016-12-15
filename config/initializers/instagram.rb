0e9d4f229f8f01a049ad3cb51ee2e3caf01351de
Steven Cook <stevenjamescook@gmail.com>
2016-12-15 09:02:17 +0000

    spike

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,44 @@
+Instagram.configure do |config|
+  config.client_id = '8d61cd1e64b44e338b93f077901b20ce'
+  config.access_token = '245561560.e029fea.953a89e625e5438494f446f5990ccf5e'
+end
+
+
+# https://api.instagram.com/oauth/authorize/?client_id=56406bc08d9c43a58538471ffbfce9d5&redirect_uri=http://localhost&response_type=code&scope=public_content
+
+
+# https://api.instagram.com/v1/tags/search.json?access_token=0fe61ea371104189b6bc8b4b9e3d9712&q=cat
+
+
+# https://instagram.com/oauth/authorize/?client_id=56406bc08d9c43a58538471ffbfce9d5&redirect_uri=http://localhost&response_type=token
+
+
+# 0fe61ea371104189b6bc8b4b9e3d9712
+
+
+# 0fe61ea371104189b6bc8b4b9e3d9712
+
+
+# https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=245561560.56406bc.574cdbb5f49e45d999b72ff84720ae61
+# https://api.instagram.com/v1/tags/malhamtarn/media/recent?access_token=245561560.e029fea.953a89e625e5438494f446f5990ccf5e
+
+
+
+
+https://api.instagram.com/v1/tags/malhamtarn?access_token=245561560.56406bc.574cdbb5f49e45d999b72ff84720ae61
+
+
+https://api.instagram.com/v1/tags/malhamtarn?access_token=245561560.e029fea.953a89e625e5438494f446f5990ccf5e
+
+https://api.instagram.com/v1/tags/search?q=malhamtarn&access_token=245561560.56406bc.574cdbb5f49e45d999b72ff84720ae61
+
+
+54.097240, -2.164670
+
+https://api.instagram.com/v1/media/search?lat=-2.164670&lng=54.097240&access_token=245561560.56406bc.574cdbb5f49e45d999b72ff84720ae61
+245561560.e029fea.953a89e625e5438494f446f5990ccf5e
+
+
+https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=245561560.e029fea.953a89e625e5438494f446f5990ccf5e
+
+https://api.instagram.com/v1/tags/lochneldricken/media/recent?access_token=245561560.e029fea.953a89e625e5438494f446f5990ccf5e
