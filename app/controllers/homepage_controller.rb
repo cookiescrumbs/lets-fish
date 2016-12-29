fb1857bef80975c14dbbbe3b5fc801dcbe7823f4
Steven Cook <stevenjamescook@gmail.com>
2016-12-29 10:20:22 +0000

    6 waters on the homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@ class HomepageController < ApplicationController
   include Homepage
 
   def index
-    @waters = Homepage::Waters::recently_added
+    @waters = Homepage::Waters::recently_added(6)
     # fresh_when @waters, public: true
     @fisheries = Homepage::Fisheries::recently_added
     # fresh_when @fisheries, public: true
