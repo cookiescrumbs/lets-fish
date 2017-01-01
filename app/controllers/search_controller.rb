49f652d028e824a5e43d3913402ea541edf4815d
Steven Cook <stevenjamescook@gmail.com>
2017-01-01 22:41:17 +0000

    search restricted to published fisheries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,11 +5,12 @@ class SearchController < ApplicationController
   end
 
   def within_bounding_box
-    waters = Water.within_bounding_box(bounds).limit 20 unless bounds.nil?
+    waters = Water.includes(:fishery).where('fisheries.published' => true).within_bounding_box(bounds).limit 20 unless bounds.nil?
     @markers = waters
     @results = waters
     render 'search'
   end
+
 
   private
 
