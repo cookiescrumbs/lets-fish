04219ec55f89662b4f2434a250b5774fc9c5ead0
Steven Cook <stevenjamescook@gmail.com>
2017-02-25 19:30:47 +0000

    added cors, header.jpg in assets folder

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'uglifier', '>= 1.3.0'
 
 gem 'font-awesome-rails'
 
+gem 'rack-cors'
+
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,18 @@ Bundler.require(:default, Rails.env)
 
 module LetsFish
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
