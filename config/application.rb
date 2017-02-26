670e427a8cf2b3abdb343c80e8346b79728aad89
Steven Cook <stevenjamescook@gmail.com>
2017-02-26 18:17:06 +0000

    cors

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,6 @@ Bundler.require(:default, Rails.env)
 module LetsFish
   class Application < Rails::Application
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
---
+++ config/initializers/rack-cors.rb
@@ -0,0 +1,16 @@
+## Configure Rack CORS Middleware, so that CloudFront can serve our assets.
+## See https://github.com/cyu/rack-cors
+
+if defined? Rack::Cors
+    Rails.configuration.middleware.insert_before 0, Rack::Cors do
+        allow do
+            origins %w[
+                https://lets.fish
+                http://lets.fish
+                https://www.lets.fish
+                http://www.lets.fish
+            ]
+            resource '/assets/*'
+        end
+    end
+end
