45ab5a162fcfaa7374ec0d748fe4ca255b938b7d
Steven <stevenjamescook@gmail.com>
2017-05-16 18:51:29 +0100

    hero images for homepage sorted

--- app/helpers/image_helper.rb
+++
@@ -1,6 +0,0 @@
-module ImageHelper
-  def responsive_image(source:, srcset: {}, options: {})
-    srcset = srcset.map { |src, size| "#{asset_path(src)} #{size}" }.join(', ')
-    image_tag(source, options.merge(srcset: srcset))
-  end
-end
