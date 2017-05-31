6ac87af23606471c84b10515e32f5bb1eeacfec9
Steven <stevenjamescook@gmail.com>
2017-05-31 11:37:22 +0100

    styles

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -10,4 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require bootstrap.min
+//= require jquery.easing.min
 //= require suggested-search.min
+//= require navigation
