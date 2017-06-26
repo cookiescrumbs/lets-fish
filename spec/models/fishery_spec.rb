484e447df4836d1694e4c82fde3c24a1a9f91d4d
Steven <stevenjamescook@gmail.com>
2017-06-26 10:21:26 +0100

    refactored tests

--- spec/models/fishery_spec.rb
+++ spec/models/fishery_spec.rb
@@ -7,32 +7,30 @@ describe Fishery, type: :model do
     create_water_types
     water_one = FactoryGirl.create(:water, name: 'z', species_ids: [1,2], water_type_id: 1)
     water_two = FactoryGirl.create(:water, name: 'a', species_ids: [1,2,3], water_type_id: 2)
-    @fishery = FactoryGirl.create(:fishery, name: 'my fishery', waters: [water_one, water_two])
+    FactoryGirl.create(:fishery, name: 'my fishery', waters: [water_one, water_two])
   end
 
   describe 'validate fields' do
     it 'is not valid without a name' do
-      @fishery.name = nil
-      expect(@fishery).not_to be_valid
+      expect(FactoryGirl.build(:fishery, name: nil)).not_to be_valid
     end
 
     it 'is not valid without a description' do
-      @fishery.description = nil
-      expect(@fishery).not_to be_valid
+      expect(FactoryGirl.build(:fishery, description: nil)).not_to be_valid
     end
   end
 
   describe 'all species from across all waters of a fishery' do
 
       it 'returns the species' do
-        expect(@fishery.species).to eql ["brown trout", "salmon", "sea trout"]
+        expect(Fishery.last.species).to eql ["brown trout", "salmon", "sea trout"]
       end
 
   end
 
   describe 'all water types across all waters of a fishery' do
       it 'returns the water types' do
-        expect(@fishery.water_types).to eql ['lake', 'river']
+        expect(Fishery.last.water_types).to eql ['lake', 'river']
       end
 
   end
@@ -48,30 +46,30 @@ describe Fishery, type: :model do
       before do
         stub_google_geocode_address
         stub_google_places_details
-        @fishery = FactoryGirl.create(:fishery)
+        FactoryGirl.create(:fishery)
       end
 
       it 'uses latitude from Google places' do
-        expect(@fishery.latitude).to eql(53.993151)
+        expect(Fishery.last.latitude).to eql(53.993151)
       end
 
       it 'uses longitude form Google places' do
-        expect(@fishery.longitude).to eql(-2.431154)
+        expect(Fishery.last.longitude).to eql(-2.431154)
       end
     end
 
     context 'has no Google place id' do
       before do
         stub_google_geocode_address
-        @fishery = FactoryGirl.create(:fishery, place_id: nil)
+        FactoryGirl.create(:fishery, place_id: nil)
       end
 
       it 'uses address model latitude' do
-        expect(@fishery.latitude).to eql(53.4807593)
+        expect(Fishery.last.latitude).to eql(53.4807593)
       end
 
       it 'Uses address model longitude' do
-        expect(@fishery.longitude).to eql(-2.2426305)
+        expect(Fishery.last.longitude).to eql(-2.2426305)
       end
     end
   end
@@ -80,7 +78,7 @@ describe Fishery, type: :model do
   describe 'geographical center of all the waters in a fishery' do
 
     it 'returns a lat,lng' do
-      expect(@fishery.geographic_center_of_waters).to eql Geocoder::Calculations.geographic_center(@fishery.waters)
+      expect(Fishery.last.geographic_center_of_waters).to eql Geocoder::Calculations.geographic_center(Fishery.last.waters)
     end
 
   end
