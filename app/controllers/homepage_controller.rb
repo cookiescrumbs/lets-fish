b233a244a97c8ea1dd1cc54eaccb20a9dd30664c
Steven <stevenjamescook@gmail.com>
2017-07-18 11:33:34 +0100

    random hero image for homepage header

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,8 @@ class HomepageController < ApplicationController
   include Lets
 
   def index
-    @waters = Lets::Waters::recently_added(6)
+    @waters = Lets::Waters::recently_added 6
+    @water = Lets::Waters::random
     # fresh_when @waters, public: true
     @fisheries = Lets::Fisheries::recently_added
     # fresh_when @fisheries, public: true
