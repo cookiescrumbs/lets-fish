2042e863529e74ee9c49ca19d8771fb1cb91f90a
Steven <stevenjamescook@gmail.com>
2017-09-03 19:56:43 +0100

    removed unwanted code

--- app/models/species.rb
+++ app/models/species.rb
@@ -3,8 +3,4 @@ class Species < ActiveRecord::Base
   has_many :waters, through: :species_waters
 
   validates :name, presence: true
-
-  # def name
-  #   super.capitalize unless super.nil?
-  # end
 end
