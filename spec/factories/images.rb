4009a5775e516f674080989873f0018c06172f2c
Steven <stevenjamescook@gmail.com>
2017-10-05 14:42:01 +0100

    don't want to remove geograph_photo_id from factory yet

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -4,6 +4,6 @@ FactoryGirl.define do
     image_file_name { 'loch.jpg' }
     image_content_type { 'image/jpeg' }
     image_file_size { 1_973_208 }
-    # geograph_photo_id { 123_456 }
+    geograph_photo_id { 123_456 }
   end
 end
