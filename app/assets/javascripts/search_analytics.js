8e8c5aa081a4c20ad10883311e440b60d90210e1
Steven Cook <steven.cook@ef.com>
2019-05-19 15:24:58 +0100

    build(chore): remove with space from dist_id

--- app/assets/javascripts/search_analytics.js
+++ app/assets/javascripts/search_analytics.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
     var input = document.getElementsByTagName("input")[0];
     input.addEventListener("input", function(event){
-        ga('send', {
-            hitType: 'search',
-            eventCategory: 'search',
-            eventAction: 'Searched',
-            eventLabel: event.srcElement.value
-        });
+        ga('send', 'event', 'Search', 'full suggested search', event.srcElement.value);
         console.log(event.srcElement.value);
     });
 });
