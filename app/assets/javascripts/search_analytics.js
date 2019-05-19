d2c9076461d781abb772311f104fb79f28c2ba32
Steven Cook <steven.cook@ef.com>
2019-05-19 06:54:59 +0100

    search analytics

--- app/assets/javascripts/search_analytics.js
+++ app/assets/javascripts/search_analytics.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     var input = document.getElementsByTagName("input")[0];
     input.addEventListener("input", function(event){
-        gtag('event', 'search', {'search_term': event.srcElement.value });
+        ga('send', {
+            hitType: 'search',
+            eventCategory: 'search',
+            eventAction: 'Searched',
+            eventLabel: event.srcElement.value
+        });
+        console.log(event.srcElement.value);
     });
 });
