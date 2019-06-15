04166509cc8af2ad80271f48a84e07776489ec79
Steven Cook <steven.cook@ef.com>
2019-06-15 15:34:24 +0100

    config.public_file_server.headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,12 +28,12 @@ LetsFish::Application.configure do
 
 
   # cache static assets for 30 days - 2592000 seconds
-  config.public_file_server.headers = {
-    'Access-Control-Allow-Origin' => ENV['ASSET_HOST'],
-    'Access-Control-Request-Method' => %w{GET OPTIONS}.join(","),
-    'Cache-Control' => 'public, max-age=2592000',
-    'Expires' => "#{1.year.from_now.to_formatted_s (:rfc822)}"
-  }
+#   config.public_file_server.headers = {
+#     'Access-Control-Allow-Origin' => ENV['ASSET_HOST'],
+#     'Access-Control-Request-Method' => %w{GET OPTIONS}.join(","),
+#     'Cache-Control' => 'public, max-age=2592000',
+#     'Expires' => "#{1.year.from_now.to_formatted_s (:rfc822)}"
+#   }
 
   config.assets.prefix = '/assets'
 
