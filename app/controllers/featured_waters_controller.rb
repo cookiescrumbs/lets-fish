a94b5b5aa1ce93a788b106554f990af0392ef6e0
Steven Cook <steven.cook@ef.com>
2019-06-28 00:02:24 +0100

    featured

--- app/controllers/featured_waters_controller.rb
+++ app/controllers/featured_waters_controller.rb
@@ -2,7 +2,7 @@ class FeaturedWatersController < ApplicationController
     include Lets
   
     def index
-      @waters = Lets::Waters::Featured(6)
+      @waters = Lets::Waters::featured(6)
     end
   end
   
