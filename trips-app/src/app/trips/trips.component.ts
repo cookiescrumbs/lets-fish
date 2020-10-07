ed57aa562c6b7947df9e6050f9f7a01763c03bcd
cookiescrumbs <stevenjamescook@gmail.com>
2020-10-07 11:28:18 +0100

    trip temp back button

--- trips-app/src/app/trips/trips.component.ts
+++ trips-app/src/app/trips/trips.component.ts
@@ -4,55 +4,58 @@ import { tap } from 'rxjs/operators';
 import { PostService } from '../services/post.service';
 import { MapService } from '../services/map.service';
 
-import { Post, Marker} from '../types';
+import { Post, Marker } from '../types';
 
 import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
-  selector: 'app-trips',
-  templateUrl: './trips.component.html',
-  styleUrls: ['./trips.component.scss']
+    selector: 'app-trips',
+    templateUrl: './trips.component.html',
+    styleUrls: ['./trips.component.scss']
 })
 export class TripsComponent implements OnInit {
 
 
-  public posts$: Observable<Post[]>;
-  public markers$: Observable<Marker[]>;
-  public currentPost$: Observable<Post>;
+    public posts$: Observable<Post[]>;
+    public markers$: Observable<Marker[]>;
+    public currentPost$: Observable<Post>;
 
-  private _tripId: string;
+    private _tripId: string;
 
-  constructor(
-      public postService: PostService,
-      public mapService: MapService,
-      private readonly route: ActivatedRoute
-  ) { }
+    constructor(
+        public postService: PostService,
+        public mapService: MapService,
+        private readonly route: ActivatedRoute
+    ) { }
 
-  public ngOnInit() {
-      this.route.paramMap.subscribe(params => {
-          const tripId = params.get('tripId');
-          if (tripId) {
-              this._tripId = tripId;
-              this._getPosts();
-              this._getMarkers();
-              console.log('tripId', this._tripId);
-          }
-      });
-  }
+    public ngOnInit() {
+        this.route.paramMap.subscribe(params => {
+            const tripId = params.get('tripId');
+            if (tripId) {
+                this._tripId = tripId;
+                this._getPosts();
+                this._getMarkers();
+            }
+        });
+    }
 
 
-  private _getPosts(): void {
-      this.posts$ = this.postService.getPosts(this._tripId)
-      .pipe(
-          tap(() => {
-              this.currentPost$ = this.postService.getCurrentPost();
-          })
-      );
-  }
+    private _getPosts(): void {
+        this.posts$ = this.postService.getPosts(this._tripId)
+            .pipe(
+                tap(() => {
+                    this.currentPost$ = this.postService.getCurrentPost();
+                })
+            );
+    }
 
-  private _getMarkers(): void {
-      this.markers$ = this.mapService.markers;
-  }
+    private _getMarkers(): void {
+        this.markers$ = this.mapService.markers;
+    }
+
+    public onClickBack(): void {
+        document.location.href = '/waters/';
+    }
 
 }
