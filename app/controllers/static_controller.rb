0d0ad099697e52ac9c9b31f88550608165808792
cookiescrumbs <stevenjamescook@gmail.com>
2021-09-24 14:43:50 +0100

    basic auth

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,16 @@
 class StaticController < ApplicationController
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  
+  before_action :authenticate
+
   def index
     render :layout => false, file: 'public/trips-app/trips.html'
+  end
+
+  private
+
+  def authenticate
+    http_basic_authenticate_or_request_with name:  ENV['TRIPS_USERNAME'],
+                                            password: ENV['TRIPS_PASSWORD']
   end
 end
