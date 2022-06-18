ffa2c53b253dab09720477dceeb37272e588c7f9
Steven Cook <stevenjamescook@gmail.com>
2022-06-18 10:56:24 +0100

    Uglifier.new(harmony: true)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ LetsFish::Application.configure do
     config.assets.prefix = '/assets'
   
     # Compress JavaScripts and CSS.
-    config.assets.js_compressor = :uglifier
+    config.assets.js_compressor = Uglifier.new(harmony: true)
     # config.assets.css_compressor = :sass
   
     # Do not fallback to assets pipeline if a precompiled asset is missed.
