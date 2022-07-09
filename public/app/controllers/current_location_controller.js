a1c5e8d8c6fd0c2076877d0291617a2cd7097022
Steven Cook <stevenjamescook@gmail.com>
2022-07-09 14:04:04 +0100

    error handling for geolocation

--- public/app/controllers/current_location_controller.js
+++ public/app/controllers/current_location_controller.js
@@ -1,17 +1,27 @@
 import { Controller } from 'https://unpkg.com/@hotwired/stimulus/dist/stimulus.js';
 
 export default class extends Controller {
-  
+  static targets = [ 'output'];
+
   submit() {
-    this.element.innerHTML = "Loading....";
+    const el = this.element;
+    el.innerHTML = 'Loading...';
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.currentPosition);
+      navigator.geolocation.getCurrentPosition(this.currentPosition, this.error);
     } else { 
       alert("Geolocation is not supported by this browser please enter a location");
     }
   }
-
+  
   currentPosition(position) {
     window.location.href = `/waters/nearby/location/${position.coords.latitude},${position.coords.longitude}/within/200`;
+  }
+
+  error() {
+    const el = document.getElementById('current-location-button');
+    el.innerHTML = '<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> Unable to retrieve your location. Please enter a postcode.'; 
+    el.classList.add('alert');
+    el.classList.add('alert-danger');
+    el.classList.remove('btn-primary');
   }
 }
